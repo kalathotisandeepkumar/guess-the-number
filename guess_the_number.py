9ef3ad3ca6567ccd3186f09f0fc7e245ec80b84e
kalathotisandeepkumar <80236236+kalathotisandeepkumar@users.noreply.github.com>
2021-04-17 11:25:36 +0530

    Create guess_the_number.py
    Sample code by using if else in python to guess the number

---
+++ guess_the_number.py
@@ -0,0 +1,35 @@
+#basic code for guessing the number by performing simple operations
+#most of my friends have said that the guessing the number will be done by using AI, machine learning etc.
+#but i have performed few mathematical operations so one can guess the number in other's mind..
+
+#Python code:
+#select the number from 1 to 9 in mind and dont say it me.
+#and then sir multiply that selected number with '2'
+#and then sir multiply that result with '5'
+#ask your friend to enter that result 
+
+
+
+n = int(input("enter the result after the mathematical operation"))
+if(n==10):
+  print("Your selected number is 1")
+elif(n==20):
+  print("Your selected number is 2")
+elif(n==30):
+  print("Your selected number is 3")
+elif(n==40):
+  print("Your selected number is 4")
+elif(n==50):
+  print("Your selected number is 5")
+elif(n==60):
+  print("Your selected number is 6")
+elif(n==70):
+  print("Your selected number is 7")
+elif(n==80):
+  print("Your selected number is 8")
+elif:
+  print("Your selected number is 9")
+else:
+  print("Please check the steps or mathematical calculation error")
+  
+                    
